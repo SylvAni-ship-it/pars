4046d4f44bfcc977efa1c9868e005f99c4b5050b
AniSylv <ap3779059@gmail.com>
2026-05-13 15:40:55 +0300

    ы

--- pars.py
+++ pars.py
@@ -3,10 +3,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+sity = input("город: ")
+prise = input("максималтная цена: ")
+
+content = {
+}
+
 custom_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
 
-response = requests.get("https://www.avito.ru/", headers=custom_headers)
+response = requests.get(f"https://m.avito.ru/{sity}/nedvizhimost", headers=custom_headers)
 
-print(response.status_code)
+soup = BeautifulSoup(response.text, "html.parser")
+
+

0fb90e2fcc209d2cc2b3b73f53461a205010b8d9
Ani <ap3779059@gmail.com>
2026-05-17 08:07:29 +0300

    ура

--- pars.py
+++ pars.py
@@ -3,8 +3,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-sity = input("город: ")
-prise = input("максималтная цена: ")
+сity = input("город: ")
+priсe = input("максималтная цена: ")
 
 content = {
 }
@@ -13,10 +13,19 @@ custom_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
 
-response = requests.get(f"https://m.avito.ru/{sity}/nedvizhimost", headers=custom_headers)
+response = requests.get(f"https://m.avito.ru/{сity}/nedvizhimost", headers=custom_headers)
+
+print("iva-item" in response.text)
+print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-res = soup.find("a", class_="styles-module-root-cfrVG styles-module-root_underlineOffset_size-m-ce9r8 styles-module-root_noVisited-U4swI styles-module-root_preset_black-VfJP4")
+for r in soup.find_all("div", class_="body-root-YwlPk"):
+    print("t")
+    
+    name = r.find("a", class_="styles-module-root-cfrVG styles-module-root_underlineOffset_size-m-ce9r8 styles-module-root_noVisited-U4swI styles-module-root_preset_black-VfJP4")
+    print(name.text)
+    print(f"https://www.avito.ru{name["href"]}")
 
-print(res.text)
+with open("test.txt", "w", encoding="utf-8" ) as f:
+    f.write(soup.prettify())
